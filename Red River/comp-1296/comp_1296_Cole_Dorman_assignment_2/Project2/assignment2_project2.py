e720da871dda5ac16faecd93a7fa93c632785bce
Icewaveeee <coledorman12@gmail.com>
2024-01-25 10:09:23 -0600

    renamed file

---
+++ Red River/comp-1296/comp_1296_Cole_Dorman_assignment_2/Project2/assignment2_project2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 26 10:36:27 2023
+
+@author: coled
+"""
+
+string1 = "Monday"
+string2 = "Tuesday"
+float1 = -17.45
+int1 = 3
+
+print(f"The temperature on {string1} was {float1}")
+contact_details = f"Contact Details: Email: {my_dict['email']}, Phone: {my_dict['phone']}"
