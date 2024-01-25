921ea993dab53420bb8a9ff51ee3aaae7461ef01
Icewaveeee <coledorman12@gmail.com>
2024-01-25 11:08:25 -0600

    minor commit

--- RRC/Comp-1701/Scripts/cd_0385010_boxstore.sql
+++ RRC/Comp-1701/Scripts/cd_0385010_boxstore.sql
@@ -4,13 +4,13 @@
 ** Date: 2024-01-24
 ** History:  YYYY-MM-DD
 **           Description and list of changes
-**           - change 1
+**           - added changelist
 **           - change 2
 **           - change n
 **
-**           2024-01-24
+**           2024-01-25
 **           Latest description and list of changes:
-**           - added changelist
+**           - 
 **/
 
 -- -------------------------------------------------------------------
@@ -23,3 +23,16 @@ CHARSET='utf8mb4'
 COLLATE='utf8mb4_unicode_ci';
 
 USE cd_0385010_boxstore;
+-- create people table
+DROP TABLE IF NOT exists people;
+CREATE TABLE IF not EXISTS people (
+  p_id MEDIUMINT AUTO_INCREMENT
+  , full_name VARCHAR(100) NOT NULL
+  , CONSTRAINT people___PK PRIMARY KEY(p_id)
+  , CONSTRAINT people___UK_fullname UNIQUE(full_name ASC)
+);
+-- import csv file into the people table
+
+LOAD DATA INFILE 'C:\Users\coled\Desktop\Github\schoolwork\RRC\Comp-1701\_data_imports.lnk'
+INTO TABLE people
+FIELDS TERMINATED BY '
