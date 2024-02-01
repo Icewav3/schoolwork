ab67ae51a244538281c57118e2f383aa0ec7e5fe
Icewaveeee <coledorman12@gmail.com>
2024-02-01 10:02:33 -0600

    update

--- RRC/Comp-1701/cd_0385010_boxstore.sql
+++ RRC/Comp-1701/cd_0385010_boxstore.sql
@@ -48,7 +48,7 @@ INSERT INTO people (fullname) VALUES
     
 
 -- import csv file into the people table
-
+/*
 LOAD DATA INFILE 'C:/_data/_imports/coledorman_0385010_boxstore_people_10000.csv'
 INTO TABLE people
 FIELDS TERMINATED BY '\n'
@@ -58,16 +58,26 @@ SELECT p_id, full_name
 FROM people
 ORDER BY p_id DESC
 LIMIT 100;
-
+*/
 ALTER TABLE people
-  ADD COLUMN first_name VARCHAR(35) NULL
-  ADD COLUMN last_name VARCHAR(35) NULL;
-
+  ADD COLUMN  IF NOT EXISTS first_name VARCHAR(35) NULL,
+  ADD COLUMN IF NOT EXISTS last_name VARCHAR(35) NULL;
+/*
 SELECT p_id, full_name
   , INSTR(full_name, ' ') AS space_loc, 1
-  , INSTR(full_name, ' ')1 AS first_name_begin
-  , INSTR(full_name, ' ')-1 AS first_name_end
-  ,INSTR(full_name, ' ')+1 AS last_name_begin
+  , INSTR(full_name, ' ') + 1 AS first_name_begin
+  , INSTR(full_name, ' ') - 1 AS first_name_end
+  , INSTR(full_name, ' ') + 1 AS last_name_begin
   , CHAR_LENGTH(full_name) AS last_name_end
-  , MID(full_name, 1, INSTR(full_name, ' ')-1) AS first_name
-  , MID(full_name, INSTR(full_name, ' ')+1), CHAR_LENGTH(full_name)-(INSTR(full_name, ' ')+1)+1)) AS last_name
+  , MID(full_name, 1, INSTR(full_name, ' ') - 1) AS first_name
+  , MID(full_name, INSTR(full_name, ' ') + 1, CHAR_LENGTH(full_name) - INSTR(full_name, ' ') - 1) AS last_name;
+*/
+UPDATE people
+SET first_name = MID(full_name, 1, INSTR(full_name, ' ') - 1)
+, last_name MID(full_name, INSTR(full_name, ' ') + 1, CHAR_LENGTH(full_name)
+WHERE 1=1
+
+SELECT p_id, full_name, first_name, last_name
+FROM people
+ORDER BY p_id
+LIMIT 2;
